4de09f2d431443b4494624445fef95456401da95
Sanjeev <swtbase@yahoo.com>
2023-01-05 22:08:02 +1100

    Added volume permissions

--- runKodiDocker.sh
+++ runKodiDocker.sh
@@ -2,9 +2,9 @@
 docker build --build-arg HOST_UID=$(id -u) -f Dockerfile -t kodi .
 
 mkdir -p ~/.config/kodi
-docker run -d --rm -p 5900:5900 -p 32400:8080 \
-           -v ~/.config/kodi:/home/kodi/.kodi \
-           -v /dev/dri/by-path:/dev/dri/by-path \
+docker run --detach --rm --publish 5900:5900 --publish 32400:8080 \
+           -v ~/.config/kodi:/home/kodi/.kodi:rw \
+           -v /dev/dri/by-path:/dev/dri/by-path:ro \
            --device /dev/dri/renderD128 \
            -e LIBVA_DRIVER_NAME=i965 -e GST_GL_WINDOW=x11 -e GST_VAAPI_DRM_DEVICE=/dev/dri/renderD128 \
            --name kodi kodi
